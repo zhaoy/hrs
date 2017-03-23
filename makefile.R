8a6fadbe6d0e360faccfe127575ce7ff23daf482
zhaoy <zhaoy@users.noreply.github.com>
2017-03-23 12:49:04 -0400

    re-factor

--- makefile.R
+++ makefile.R
@@ -1,23 +1,32 @@
 # Load packages.
 
-library(package = rprojroot)
+library(package = rprojroot) # find absolute file-path of root directory
+library(package = purrr)     # functional programming tools
 
 # Locate code.
 
 root_path <- find_root(criterion = "README.md",
                        path = ".")
 
-setwd(dir = root_path)
+# Run code.
 
-source(file = "code/1_hrs_respondents.R")
-source(file = "code/2_hrs_variables.R")
-source(file = "code/3_hrs_interviews.R")
-source(file = "code/4_hrs_variables_respondents.R")
+hrs_code <- c("code/1_hrs_respondents.R",
+              "code/2_hrs_variables.R",
+              "code/3_hrs_interviews.R",
+              "code/4_hrs_variables_respondents.R")
 
-# Remove intermediate files.
+map(.x = hrs_code,
+    .f = source,
+    local = FALSE,
+    echo = FALSE,
+    print.eval = FALSE,
+    verbose = FALSE,
+    chdir = TRUE)
 
-intermediates <- c("1_hrs_respondents.tsv", # 1_hrs_respondents.R
-	                 "2_hrs_variables.tsv",   # 2_hrs_variables.R
-	                 "3_hrs_interviews.tsv")  # 3_hrs_interviews.R
+# Remove intermediate data.
 
-file.remove(intermediates)
+hrs_intermediates <- c("1_hrs_respondents.tsv", # 1_hrs_respondents.R
+	                     "2_hrs_variables.tsv",   # 2_hrs_variables.R
+	                     "3_hrs_interviews.tsv")  # 3_hrs_interviews.R
+
+file.remove(hrs_intermediates)
