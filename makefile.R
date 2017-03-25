bb9e8f028255e80d8279bf2e3abcde025c72ff49
zhaoy <zhaoy@users.noreply.github.com>
2017-03-25 11:01:30 -0400

    add 5_hrs

--- makefile.R
+++ makefile.R
@@ -13,7 +13,8 @@ root_path <- find_root(criterion = "README.md",
 hrs_code <- c("code/1_hrs_respondents.R",
               "code/2_hrs_variables.R",
               "code/3_hrs_interviews.R",
-              "code/4_hrs_variables_respondents.R")
+              "code/4_hrs_variables_respondents.R",
+              "code/5_hrs.R")
 
 map(.x = hrs_code,
     .f = source,
@@ -22,10 +23,11 @@ map(.x = hrs_code,
     verbose = FALSE,
     chdir = TRUE)
 
-# Remove intermediate data.
+# Remove intermediate files.
 
 hrs_intermediates <- c("1_hrs_respondents.tsv", # 1_hrs_respondents.R
                        "2_hrs_variables.tsv",   # 2_hrs_variables.R
-                       "3_hrs_interviews.tsv")  # 3_hrs_interviews.R
+                       "3_hrs_interviews.tsv",  # 3_hrs_interviews.R
+                       "code/5_hrs.html")       # 5_hrs.Rmd
 
 file.remove(hrs_intermediates)
