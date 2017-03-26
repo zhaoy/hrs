936a3533e20018d30ed9c19939855dc403ad0a03
zhaoy <zhaoy@users.noreply.github.com>
2017-03-26 00:22:43 -0400

    add 5_hrs

---
+++ code/5_hrs.R
@@ -0,0 +1,19 @@
+# Load packages.
+
+library(package = rprojroot) # find absolute file-path of root directory
+library(package = rmarkdown) # dynamic documents
+
+# Locate code.
+
+root_path <- find_root(criterion = "README.md",
+                       path = ".")
+
+render(input = "5_hrs.Rmd",
+       output_format = NULL,
+       output_file = NULL,
+       output_dir = root_path,
+       intermediates_dir = NULL,
+       knit_root_dir = root_path,
+       runtime = "auto",
+       clean = TRUE,
+       quiet = TRUE)
