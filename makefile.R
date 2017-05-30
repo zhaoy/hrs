b9795abe3e4983bc4a909e1fa082a1eb37dc3b1e
zhaoy <zhaoy@users.noreply.github.com>
2017-05-29 20:12:07 -0400

    edit

--- makefile.R
+++ makefile.R
@@ -1,6 +1,6 @@
 # Load packages.
 
-library(package = rprojroot) # find absolute file-path of root directory
+library(package = rprojroot) # find files in sub-directories
 library(package = purrr)     # functional programming tools
 
 # Locate code.
@@ -13,21 +13,15 @@ root_path <- find_root(criterion = "README.md",
 hrs_code <- c("code/1_hrs_respondents.R",
               "code/2_hrs_variables.R",
               "code/3_hrs_interviews.R",
-              "code/4_hrs_variables_respondents.R",
-              "code/5_hrs.R")
+              "code/4_hrs.R")
 
 map(.x = hrs_code,
-    .f = source,
-    local = FALSE,
-    echo = FALSE,
-    verbose = FALSE,
-    chdir = TRUE)
+    .f = source)
 
 # Remove intermediate files.
 
 hrs_intermediates <- c("1_hrs_respondents.tsv", # 1_hrs_respondents.R
                        "2_hrs_variables.tsv",   # 2_hrs_variables.R
-                       "3_hrs_interviews.tsv",  # 3_hrs_interviews.R
-                       "code/5_hrs.html")       # 5_hrs.Rmd
+                       "3_hrs_interviews.tsv")  # 3_hrs_interviews.R
 
 file.remove(hrs_intermediates)
